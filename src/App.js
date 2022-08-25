bb55333d741dbf3dfcb8bf7eb0f610177beb5b0b
hugo taniady <hugotani97@gmail.com>
2022-08-25 11:48:12 +0700

    First Commit

---
+++ src/App.js
@@ -0,0 +1,94 @@
+import WeatherComponent from "./modules/WeatherInfoComponent";
+import styled from "styled-components";
+
+import React, { useState } from 'react';
+const api = {
+  key: "578be37677c1c2d2eaa5d8dc5e6499a5",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+
+function App() {
+
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState('');
+  const [city, updateCity] = useState();
+
+  const search = evt => {
+    if (evt.key === "Enter"){
+      fetch(`${api.base}weather?q=${query}&units=metric&appID=${api.key}`)
+      .then(res => res.json())
+      .then(result => {
+        setWeather(result);
+        setQuery('');
+        // console.log(result);
+      });
+    }
+  }
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+  return (
+    <div className={
+      (typeof weather.main != "undefined") ? ((weather.main.temp > 24) ? 'app warm' : 'app') : 'app'}>
+        <main>
+          <div className='search-box'>
+            <input 
+            type="text" 
+            className="search-bar" 
+            placeholder="Enter Location" 
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}/>
+          </div>
+            {(typeof weather.main !="undefined") ? (
+              <div>
+                <div className='location-box'>
+              <div className='location'>{weather.name}, {weather.sys.country}</div>
+              <div className='date'>{dateBuilder(new Date())}</div>
+            </div>
+          <div className='weather-box'>
+            <div className='temp'>
+              {Math.round(weather.main.temp)}°C
+            </div>
+            <div className='weather'>
+              {weather.weather[0].main}
+            </div>
+          </div>
+          <WeatherComponent weather={weather} city={city} />
+          {/* <div className="bottom">
+            <div className="feels">
+              {weather.main ? <p className='bold'>{weather.main.feels_like.toFixed()}°F</p> : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {weather.main ? <p className='bold'>{weather.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {weather.wind ? <p className='bold'>{weather.wind.speed.toFixed()} MPH</p> : null}
+              <p>Wind Speed</p>
+            </div>
+          </div> */}
+              </div>
+            ): (
+              <div>
+                <p className='city'>Find Weather of your city</p>
+              </div>
+            )}
+        </main>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/modules/WeatherInfoComponent.js
@@ -0,0 +1,117 @@
+import React from "react";
+import styled from "styled-components";
+
+export const WeatherInfoIcons = {
+    sunset: "../icons/temp.svg",
+    sunrise: "../../icons/temp.svg",
+    humidity: "../icons/humidity.svg",
+    wind: "../icons/wind.svg",
+    pressure: "../icons/pressure.svg",
+};
+const Location = styled.span`
+  margin: 15px auto;
+  text-transform: capitalize;
+  font-size: 28px;
+  font-weight: bold;
+`;
+const Condition = styled.span`
+  margin: 20px auto;
+  text-transform: capitalize;
+  font-size: 14px;
+  & span {
+    font-size: 28px;
+  }
+`;
+const WeatherInfoLabel = styled.span`
+  margin: 20px 25px 10px;
+  text-transform: capitalize;
+  text-align: start;
+  width: 95%;
+  color: #fff;
+  font-weight: bold;
+  font-size: 1.2rem;
+`;
+const WeatherIcon = styled.img`
+  width: 100px;
+  height: 100px;
+  margin: 5px auto;
+`;
+const WeatherContainer = styled.div`
+  display: flex;
+  width: 100%;
+  margin: 30px auto;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const WeatherInfoContainer = styled.div`
+  display: flex;
+  width: 90%;
+  background-color: rgba(255, 255, 255, 0.5);
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  margin: 1rem;
+  padding-left: 10%;
+  padding-right: 10%;
+  border-radius: 8px;
+`;
+const InfoContainer = styled.div`
+  display: flex;
+  margin: 5px 10px;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+const InfoIcon = styled.img`
+  width: 36px;
+  height: 36px;
+`;
+const InfoLabel = styled.span`
+  display: flex;
+  flex-direction: column;
+  font-size: 1rem;
+  margin: 15px;
+  color: #fff;
+  & span {
+    font-size: 12px;
+    text-transform: capitalize;
+  }
+`;
+
+const WeatherInfoComponent = (props) => {
+    const {name, value} = props;
+    return (
+        <InfoContainer>
+            <InfoIcon src={WeatherInfoIcons[name]}/>
+            <InfoLabel>
+                {value}
+                <span>{name}</span>
+            </InfoLabel>
+        </InfoContainer>
+    );
+};
+const WeatherComponent = (props) => {
+    const {weather} = props;
+    const isDay = weather?.weather[0].icon?.includes('d')
+    const getTime = (timeStamp) => {
+        return `${new Date(timeStamp * 1000).getHours()} : ${new Date(timeStamp * 1000).getMinutes()}`
+    }
+    return (
+        <>
+            
+            <WeatherInfoContainer>
+            <WeatherInfoLabel>Weather Info</WeatherInfoLabel>
+                <WeatherInfoComponent name={isDay ? "sunset" : "sunrise"}
+                                      value={`${getTime(weather?.sys[isDay ? "sunset" : "sunrise"])}`}/>
+                <WeatherInfoComponent name={"humidity"} value={weather?.main?.humidity}/>
+                <WeatherInfoComponent name={"wind"} value={weather?.wind?.speed}/>
+                <WeatherInfoComponent name={"pressure"} value={weather?.main?.pressure}/>
+            </WeatherInfoContainer>
+        </>
+    );
+};
+
+export default WeatherComponent;
