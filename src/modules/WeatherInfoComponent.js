553e139f68c0baf468b76909111b159d3d83a285
hugo taniady <hugotani97@gmail.com>
2022-09-24 15:39:41 +0700

    Update Weather Components

--- src/modules/WeatherInfoComponent.js
+++ src/modules/WeatherInfoComponent.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 export const WeatherInfoIcons = {
     sunset: "../icons/temp.svg",
-    sunrise: "../../icons/temp.svg",
+    sunrise: "../icons/temp.svg",
     humidity: "../icons/humidity.svg",
     wind: "../icons/wind.svg",
     pressure: "../icons/pressure.svg",
